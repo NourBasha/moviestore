6819c12f01061e3847e2f7ba8b588ed9182a78bc
NourB <nour.basha2011@gmail.com>
2020-09-10 20:23:33 +0200

    few changes before carousel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,11 @@
   <div class="home">
       <div id="carousel-id" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="" alt="First slide">
+    <div class="carousel-item carousel-one active">
     </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="" alt="Second slide">
+    <div class="carousel-item carousel-two">
     </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="" alt="Third slide">
+    <div class="carousel-item carousel-three">
     </div>
   </div>
 </div>
@@ -28,3 +25,29 @@ export default {
 };
 
 </script>
+
+<style lang="scss" scoped>
+
+  .home {
+    .carousel-item {
+          background: no-repeat center center scroll;
+      background-size: cover;
+      -webkit-background-size: cover ;
+      -moz-background-size: cover ;
+      -o-background-size: cover ;
+    }
+.slider .carousel-one {
+    background-image: url("../imgs/slider01.jpg");
+     
+  }
+  .slider .carousel-two {
+    background-image: url("../imgs/slider02.jpg");
+
+  }
+  .slider .carousel-three {
+    background-image: url("../imgs/slider03.jpg");
+
+  } 
+  }
+
+</style>
