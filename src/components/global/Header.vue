5e6554381bb44d6d71c09154d94e1b9b509136bc
NourB <nour.basha2011@gmail.com>
2020-09-24 17:10:18 +0200

    gh pages test

--- src/components/global/Header.vue
+++ src/components/global/Header.vue
@@ -4,20 +4,20 @@
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
  
   
-   <router-link to="/"> <a class="navbar-brand ml-2 mr-2 " id="brand-logo" href="#">Movie<span>store</span>
- 
-  </a></router-link> 
-  <ul class="navbar-nav  top-pages d-flex justiy-content-end ">
-      <li class="nav-item mr-2 active">
-        <a class="nav-link page-selector home-selector"> <router-link to="/">Home</router-link> <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item  mr-2  ">
-        <a class="nav-link page-selector home-selector"><router-link to="/browse">Movies</router-link></a>
-      </li>
-    </ul>
-  
-</nav>
-    </div>
+        <router-link to="/"> <a class="navbar-brand ml-2 mr-2 " id="brand-logo" href="#">Movie<span>store</span>
+        </a></router-link> 
+
+        <ul class="navbar-nav  top-pages flex-row ml-auto ">
+            <li class="nav-item mr-2 active">
+                <a class="nav-link page-selector home-selector"> <router-link to="/">Home</router-link> <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item  mr-2  ">
+                <a class="nav-link page-selector home-selector"><router-link to="/browse">Movies</router-link></a>
+            </li>
+            </ul>
+        
+        </nav>
+   </div>
 </template>
 
 
@@ -36,7 +36,3 @@ export default {
     }
 }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
