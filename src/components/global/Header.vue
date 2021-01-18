296e3d0a10e4a6fbe64fc8e0fc49f6bd542562e9
NourB <nour.basha2011@gmail.com>
2021-01-18 02:32:54 +0200

    production 1

--- src/components/global/Header.vue
+++ src/components/global/Header.vue
@@ -2,19 +2,25 @@
 <template>
     <div class="header" ref="mynavbar">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
- 
   
-        <router-link to="/"> <a class="navbar-brand ml-2 mr-2 " id="brand-logo" href="#">Movie<span>store</span>
-        </a></router-link> 
+        <router-link to="/"> 
+        <a class="navbar-brand ml-2 mr-2 " id="brand-logo" href="#">Movie<span>store</span>
+        </a>
+        </router-link> 
 
         <ul class="navbar-nav  top-pages flex-row ml-auto ">
             <li class="nav-item mr-2 active">
-                <a class="nav-link page-selector home-selector"> <router-link to="/">Home</router-link> <span class="sr-only">(current)</span></a>
+                <a class="nav-link page-selector home-selector"> 
+                    <router-link to="/">Home</router-link> 
+                    <span class="sr-only">(current)</span>
+                </a>
             </li>
+
             <li class="nav-item  mr-2  ">
-                <a class="nav-link page-selector home-selector"><router-link to="/browse">Movies</router-link></a>
+                <a class="nav-link page-selector movie-selector"><router-link to="/browse">Movies</router-link></a>
             </li>
-            </ul>
+
+        </ul>
         
         </nav>
    </div>
